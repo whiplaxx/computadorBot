1b8bd7a16e4ed1b7538fa057c66f44e1d3781bf7
Whiplax <whiplaxx@gmail.com>
2021-01-19 22:48:38 -0300

    Add files via upload

--- letterbox.py
+++ letterbox.py
@@ -7,6 +7,32 @@ from bs4 import BeautifulSoup
 lb_url = "https://letterboxd.com"
 lbSearchFilms_url = "https://letterboxd.com/search/films/"
 lbMovie_url = "https://letterboxd.com/film/"
+lbMovieRate_url = ["https://letterboxd.com/csi", "/rating-histogram/"]
+
+def getMovieDetails( movieName ):
+	
+	r = requests.get( lb_url + movieName )
+	if(r.status_code != 200):
+		return -1
+
+	soup = BeautifulSoup( str(r.content), 'html.parser' )
+	section = soup.find('section', id="featured-film-header")
+
+	movieDetails = {}
+	movieDetails['name'] = section.find('h1').get_text()
+	movieDetails['year'] = section.find('p').find('small', class_="number").find('a').get_text()
+	movieDetails['director'] = section.find('p').find('small').find_next_siblings()[0].find('span').get_text()
+
+	## Getting the rate
+	rateUrl = lbMovieRate_url[0] + movieName + lbMovieRate_url[1]
+	r = requests.get( rateUrl )
+	if(r.status_code != 200):
+		print(movieName)
+		print(rateUrl)
+		return -1
+	soup = BeautifulSoup( str(r.content), 'html.parser')
+	movieDetails['starsRate'] = soup.find('a', class_="tooltip display-rating").get_text()
+	return movieDetails
 
 def getMovieFromSearch( contentSearch, indexResult = 0 ):
 	
@@ -15,8 +41,7 @@ def getMovieFromSearch( contentSearch, indexResult = 0 ):
 	listOfResults = soup.find('ul', class_="results")
 	results = listOfResults.find_all('li')
 	
-	lbMovieResult_url = lb_url + results[indexResult].find('div')['data-target-link']
-	return lbMovieResult_url
+	getMovieDetails( results[indexResult].find('div')['data-target-link'] )
 
 def searchMovie( search_str, indexResult = 0 ):
 	
@@ -34,7 +59,12 @@ def searchMovie( search_str, indexResult = 0 ):
 
 if __name__ == '__main__':
 	
-	movieSearch = input('Movie: ')
+	#movieSearch = input('Movie: ')
+	
+	filePage = open('output.html', 'r')
+	content = filePage.read()	
+	filePage.close()
+	
+	getMovieFromSearch( str(content) )
 
-	movie_url = searchMovie( movieSearch )
-	print( movie_url )
+	
